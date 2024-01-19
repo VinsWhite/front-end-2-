615bbfc66180847f05e69d303918e26b98bc2825
Vincenzo <vincenzosa26@icloud.com>
2024-01-19 14:42:29 +0100

    Esercizio 32-33

--- S11L1/jobs-searchEngine/src/actions/index.js
+++ S11L1/jobs-searchEngine/src/actions/index.js
@@ -1,16 +1,22 @@
 import { endpointSearch } from "../endpoint/endpoint"
 import axios from 'axios'
 
+//azione per rimuovere dai preferiti
 export const removeFromFavourite = (fav) => {
-    return { type: "REMOVE_FROM_FAVOURITE", payload: fav}
+    return { type: "REMOVE_FROM_FAVOURITE", payload: fav} //avvia l'azione sul payload 
 }
+
+export const setJobs = (jobs) => ({
+  type: 'SET_JOBS',
+  payload: jobs,
+});
 
 export const searchJob = (query) => async (dispatch) => {
   try {
     const response = await axios.get(endpointSearch + query + "&limit=20");
     if (response.status === 200) {
       const { data } = response.data;
-      console.log("Jobs data:", data); // Aggiungi questo log per debug
+      console.log("Jobs data:", data); // Aggiungo questo log per debug
       dispatch(setJobs(data));
     } else {
       alert("Error fetching results");
@@ -20,8 +26,3 @@ export const searchJob = (query) => async (dispatch) => {
   }
 }
 
-
-export const setJobs = (jobs) => ({
-    type: 'SET_JOBS',
-    payload: jobs,
-});
--- S11L1/jobs-searchEngine/src/pages/HomePage.jsx
+++ S11L1/jobs-searchEngine/src/pages/HomePage.jsx
@@ -5,18 +5,18 @@ import { searchJob } from "../actions";
 import { useDispatch, useSelector } from "react-redux";
 
 const HomePage = () => {
-  const [query, setQuery] = useState(""); //inizializziamo lo stato
-  const dispatch = useDispatch();
-  const { jobs } = useSelector((state) => state);
+  const [query, setQuery] = useState(""); //inizializziamo lo stato di query
+  const dispatch = useDispatch(); //il dispatch è usato per passare l'oggetto all'action 
+  const { jobs } = useSelector((state) => state); //inizializziamo lo stato di jobs
 
 
-  const handleChange = e => {
+  const handleChange = e => { 
     setQuery(e.target.value);
   };
 
   const handleSubmit = async e => {
     e.preventDefault();
-    dispatch(searchJob(query))
+    dispatch(searchJob(query)) //dispatch di un azione per ricercare i lavori
   };
 
   return (
